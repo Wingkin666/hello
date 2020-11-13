3f22604e594c9998459d2aec232794583bc6da32
Wingkin <1227012915@qq.com>
2020-11-13 17:58:07 +0800

    测试对象属性的修改

--- src/test/java/com/example/hello/test.java
+++ src/test/java/com/example/hello/test.java
@@ -1,7 +1,12 @@
 package com.example.hello;
 
-public class test {
-    public static void main(String[] args) {
 
+public class test {
+
+    public static void main(String[] args) {
+        PeopleImpl peopleImpl = new PeopleImpl();
+        People people = new People();
+        peopleImpl.preExecute(people);
+        System.out.println(people.toString());
     }
 }
