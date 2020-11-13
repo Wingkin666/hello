6d1a5d53a4d91e8853bc5c738c61fd9150cd4ce5
Wingkin <1227012915@qq.com>
2020-11-13 17:22:46 +0800

    测试

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.example.hello.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @RequestMapping("/hello")
     public String HelloWorld() {
-        return "Hello World!";
+        return "rank";
     }
 }
